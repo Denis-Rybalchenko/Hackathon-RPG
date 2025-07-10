65670c0fce1cb06557b2ff9d2d55335250d56712
Denis-Rybalchenko <denis.rybalchenko.2009@gmail.com>
2025-07-10 14:21:56 +0200

    cmit

--- Program.cs
+++ Program.cs
@@ -4,9 +4,45 @@ Console.WriteLine();
 Console.WriteLine("Welcome to Command Line RPG.");
 Console.WriteLine();
 
-int i = 1;
-   foreach (string action in Menu)
+bool menuBoot = true;
+
+
+while (menuBoot == true)
+{
+    int i = 1;
+    foreach (string action in Menu)
    {
        Console.WriteLine("[" + i + "]" + " " + action);
        i++;
    }
+
+menuBoot = false;
+Console.WriteLine();
+
+
+string UserChoice = Console.ReadLine();
+
+bool test = int.TryParse(UserChoice, out int actionChoice);
+
+if (test == true)
+{
+switch (actionChoice)
+{
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        break;
+}
+
+
+}
+else
+{
+    Console.WriteLine("Invalid input try again");
+    menuBoot = true;
+}
+}
