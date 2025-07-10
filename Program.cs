6e57d50462f20c2f9bca38e884032e4d6968ff09
Denis-Rybalchenko <denis.rybalchenko.2009@gmail.com>
2025-07-10 14:02:31 +0200

    commit3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿List<string> Menu = new List<string> {"Movement","Stats", "Inventory"},
+﻿List<string> Menu = new List<string> {"Movement","Stats", "Inventory", "Exit game"};
 
+Console.WriteLine();
 Console.WriteLine("Welcome to Command Line RPG.");
+Console.WriteLine();
+
+int i = 1;
+   foreach (string action in Menu)
+   {
+       Console.WriteLine("[" + i + "]" + " " + action);
+       i++;
+   }
